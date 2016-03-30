bde18034f2c1e141f5dfbc781f471f677cff6de2
mohebifar <netfars@gmail.com>
2016-03-30 16:50:35 +0430

    fix(bin): Fix bin file with NODE_PATH env variable

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,4 @@
+#!/usr/bin/env node
+
 require('babel-polyfill');
 require('../lib');
---
+++ bin/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+NODE_PATH="$SCRIPT_PATH/../lib" node "$SCRIPT_PATH/index.js"
