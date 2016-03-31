12a6818d4499f128fbbc1fe12c93ab3e89b4a0b2
mohebifar <netfars@gmail.com>
2016-03-31 15:30:29 +0430

    refactor(indent): Fix indentation

--- src/redux/create.js
+++ src/redux/create.js
@@ -4,10 +4,10 @@ import reducers from './reducers';
 import sagas from './sagas';
 
 export default function create() {
-    const sagaMiddleware = createSagaMiddleware(sagas);
-    const middleware = compose(
-        applyMiddleware(sagaMiddleware)
-    );
+  const sagaMiddleware = createSagaMiddleware(sagas);
+  const middleware = compose(
+    applyMiddleware(sagaMiddleware)
+  );
 
-    return createStore(reducers, middleware);
+  return createStore(reducers, middleware);
 }
